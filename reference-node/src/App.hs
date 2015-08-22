355fd8a2d485d153ce6db362af06650af623ad9b
Julian K. Arni <jkarni@gmail.com>
2015-08-22 14:31:16 +0200

    Start persistence

--- reference-node/src/App.hs
+++ reference-node/src/App.hs
@@ -4,6 +4,8 @@ module App where
 import           Control.Concurrent
 import           Control.Monad.IO.Class
 import           Control.Monad.Trans.Either
+import           Data.Aeson (encode, decode)
+import qualified Data.ByteString.Lazy as BS
 import           Network.Wai
 import           Servant
 import           Servant.Docs
@@ -33,3 +35,9 @@ postIp mvar ip =
 -- | Get documentation describing the server API.
 apiDocs :: EitherT ServantErr IO Markdown
 apiDocs = return . Markdown . markdown $ docs ipManager'
+
+persistNodes :: FilePath -> [Node] -> IO ()
+persistNodes fp ns = BS.writeFile fp (encode ns)
+
+loadNodes :: FilePath -> IO (Maybe [Node])
+loadNodes fp = decode <$> BS.readFile fp
