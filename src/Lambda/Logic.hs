86668b0654b51596c8e371cee8dc656a1144a340
Julian K. Arni <jkarni@gmail.com>
2015-09-01 12:07:40 -0700

    implement server

--- src/Lambda/Logic.hs
+++ src/Lambda/Logic.hs
@@ -19,7 +19,7 @@ instance FromJSON Term
 type Env = Map.Map String Term
 
 eval :: Env -> Term -> Term
-eval env (App (Lambda s t, t2)) = let t2' = eval env t2 in eval (Map.insert s t2 env) t
-eval env (App (_, _)) = error "applying to non-lambda"
+eval env (App (Lambda s t, t2)) = let t2' = eval env t2 in eval (Map.insert s t2' env) t
+eval _ (App (_, _)) = error "applying to non-lambda"
 eval env (Var x) = fromJust $ Map.lookup x env
-eval env (Lambda s t) = Lambda s t
+eval _ (Lambda s t) = Lambda s t
--- src/Lambda/Server.hs
+++ src/Lambda/Server.hs
@@ -3,7 +3,16 @@ module Lambda.Server where
 import Lambda.Api
 import Lambda.Logic
 import Control.Monad.Trans.Either
+import Network.Wai.Handler.Warp
 import Servant
+
+
+lambdaServer :: Server LambdaApi
+lambdaServer = var
+          :<|> lambda
+          :<|> app
+          :<|> eval'
+
 
 var :: String -> EitherT ServantErr IO Term
 var = return . Var
@@ -13,3 +22,10 @@ lambda s = return . Lambda s
 
 app :: (Term, Term) -> EitherT ServantErr IO Term
 app = return . App
+
+eval' :: Term -> EitherT ServantErr IO Term
+eval' t = return $ eval mempty t
+
+runServer :: Port -> IO ()
+runServer p = run p $ serve lambdaApi lambdaServer
+
