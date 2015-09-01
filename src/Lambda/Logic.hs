c1d722d71f5b51a3f147b35a175329344632c556
Julian K. Arni <jkarni@gmail.com>
2015-09-01 11:52:21 -0700

    eval

--- src/Lambda/Logic.hs
+++ src/Lambda/Logic.hs
@@ -1,15 +1,17 @@
-
 module Lambda.Logic where
+import Data.Maybe (fromJust)
+import qualified Data.Map as Map
 
 data Term = Var String
           | Lambda String Term
           | App (Term, Term)
+    deriving (Show, Eq)
 
 
-data Scope
+type Env = Map.Map String Term
 
-eval :: Scope -> Term -> Term
-eval s (App (Lambda s t), t2)
-
-{-fn = let   -}
-    {-(c1 :<|> c2) =  client api url-}
+eval :: Env -> Term -> Term
+eval env (App (Lambda s t, t2)) = let t2' = eval env t2 in eval (Map.insert s t2 env) t
+eval env (App (_, _)) = error "applying to non-lambda"
+eval env (Var x) = fromJust $ Map.lookup x env
+eval env (Lambda s t) = Lambda s t
